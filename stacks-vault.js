df56a833d0b4130502b8ad8eb59d3a3003b6692b
Gojo <vishalvc16vc@gmail.com>
2026-02-28 23:18:50 +0530

    Add files via upload

---
+++ stacks-vault.js
@@ -0,0 +1,6 @@
+import { openContractCall } from "@stacks/connect";
+import { uintCV } from "@stacks/transactions";
+
+export async function demoTx() {
+  console.log("Stacks integration active");
+}
